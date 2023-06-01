2d0d4e343204ea8498ad59a7b79c223f64543853
vinodkumar2704 <vinodkumar.batthala@qapita.com>
2023-06-01 09:30:37 +0530

    added husky and prettier and eslint

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,15 @@ import './styles.css'
 import IMAGE from './im.png'
 import { ClickCounter } from './ClickCounter'
 
-export const App =()=>{
-    const num = 0
-    return <>
-    <h1>react webpack course - {process.env.NODE_ENV}</h1><img src={IMAGE} alt='img-logo'/>
-    <ClickCounter/>
+export const App = () => {
+  return (
+    <>
+      <h1>
+        react webpack course - {process.env.NODE_ENV}
+        {process.env.name}
+      </h1>
+      <img src={IMAGE} alt="img-logo" />
+      <ClickCounter />
     </>
+  )
 }
--- webpack/webpack.prod.js
+++ webpack/webpack.prod.js
@@ -3,9 +3,9 @@ const webpack = require('webpack')
 module.exports = {
   mode: 'production',
   devtool: 'source-map',
-//   plugins: [
-//     new webpack.DefinePlugin({
-//       'process.env.name': JSON.stringify('Codevolution'),
-//     }),
-//   ],
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env.name': JSON.stringify('Codevolution'),
+    }),
+  ],
 }
